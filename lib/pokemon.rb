50d24efed24534c00c8882b4c177ce2d64a6d08a
RochelLevi <rochlevi@gmail.com>
2017-11-06 17:31:11 -0500

    woot woot

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,19 @@
 class Pokemon
 
-  attr_accessor :name, :type, :db, :id
+  attr_accessor :name, :type, :db, :id, :hp
 
   def initialize(attributes)
+    @hp = nil
     attributes.each{|key, value| self.send(("#{key}="), value)}
   end
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (\"#{name}\", \"#{type}\")")
+  end
+
+  def alter_hp(hp, db)
+    self.hp = hp
+    db.execute("UPDATE pokemon SET hp = #{hp} WHERE id = #{self.id}")
   end
 
   def self.find(id, db)
@@ -16,6 +22,7 @@ class Pokemon
     hash["id"] = attributes[0][0]
     hash["name"] = attributes[0][1]
     hash["type"] = attributes[0][2]
+    hash["hp"] = attributes[0][3]
     Pokemon.new(hash)
   end
 end
